ed47ac55302a6168a9bb8f6d06b52c22ce61da8c
Elena Polyakova <epolyk1970@gmail.com>
2025-09-06 10:37:45 +0300

    add all fales

---
+++ src/server.js
@@ -0,0 +1,50 @@
+// src/server.js
+
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { getEnvVar } from './utils/getEnvVar.js';
+import {
+  getContactsController,
+  getContactByIdController,
+} from './controllers/contacts.js';
+
+dotenv.config();
+
+const PORT = Number(getEnvVar('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', getContactsController);
+  app.get('/contacts/:contactId', getContactByIdController);
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
