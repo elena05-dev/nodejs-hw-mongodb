86a848bfc3331e527c73d21a2882fa43c5a5212e
Elena Polyakova <epolyk1970@gmail.com>
2025-10-10 23:22:04 +0300

    err

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -73,7 +73,21 @@ export const getContactByIdController = async (req, res, next) => {
 
 export const createContactController = async (req, res, next) => {
   try {
-    const contact = await createContact(req.body, req.user._id);
+    const photo = req.file;
+    let photoUrl;
+
+    if (photo) {
+      if (getEnvVar('ENABLE_CLOUDINARY') === 'true') {
+        photoUrl = await saveFileToCloudinary(photo);
+      } else {
+        photoUrl = await saveFileToUploadDir(photo);
+      }
+    }
+
+    const contact = await createContact(
+      { ...req.body, photo: photoUrl },
+      req.user._id,
+    );
 
     res.status(201).json({
       status: 201,
