26ddc7c60b3366d9ec74f03da8e4d2db9ee51eb3
Elena Polyakova <epolyk1970@gmail.com>
2025-10-16 18:19:42 +0300

    doc Rredocly

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -21,6 +21,7 @@ router.use(authenticate);
 
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+
 router.post(
   '/',
   upload.single('photo'),
